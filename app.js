8a6d7d17c47803aca982e355ae5220549ffecb32
Caique Bezerra <caique.bezerra@gmail.com>
2021-06-15 20:49:19 -0300

    criados rotaCadastro, CadastroController e adicionados métrodo Post nos formulários Login e Cadastro

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ const port = 3000;
 const rotasIndex = require('./routes/rotasIndex');
 const rotasHome = require('./routes/rotasHome');
 const rotasLogin = require('./routes/rotasLogin');
+const rotasCadastro = require('./routes/rotasCadastro');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 app.get('/', (req, res) => {res.render ('home');});
-app.get('/login/', (req, res) => {res.render ('login');});
-app.get('/cadastro/', (req, res) => {res.render ('cadastro');});
-app.get('/index/', (req, res) => {res.render ('index');});
+app.get('/login', (req, res) => {res.render ('login');});
+app.get('/cadastro', (req, res) => {res.render ('cadastro');});
+app.get('/index', (req, res) => {res.render ('index');});
 
 app.use('/index', rotasIndex);
 app.use('/', rotasHome);
 app.use('/login', rotasLogin);
+app.use('/cadastro', rotasCadastro);
 
 app.listen(port, () => {
     console.log('Server started on port: ' + port)
--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,7 +1,23 @@
 const LoginController = {
-    /* verRecuperarSenha: (req, res) => {
+    verRecuperarSenha: (req, res) => {
         return res.render('loginForgotPsw');
-    }, */
+    },
+
+    autenticaUsuario: (req, res) => {
+        /* if (inputemail.value == "caique.bezerra@gmail.com" && inputsenha.value == "1234") {
+            alert("Acesso permitido");
+    
+          } else {
+    
+            alert("Acesso negado");
+    
+          }
+
+           let { email, senha } = req.body  */
+        res.redirect('/index');
+        console.log(req.body);
+    },
+
 }
 
 module.exports = LoginController;
