8818c77edd1b521c4ae4a2bc21deb9512d50ddb6
Aline <aline.dasilva87@yahoo.com>
2021-07-05 18:45:28 -0300

    modificando rotas para cadastro de cartões e página com cartão já cadastrado

--- routes/rotasIndex.js
+++ routes/rotasIndex.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const IndexController = require('../controllers/IndexController');
 
 router.get('/cartoes', IndexController.verCartoes);
-router.post('/cartoes', IndexController.guardarCartao);
+router.get('/cadastro-cartoes',IndexController.cadastraCartoes);
+router.post('/cadastro-cartoes', IndexController.guardarCartao);
 router.get('/transacoes', IndexController.verTransacoes);
 router.get('/entradas', IndexController.verEntradas);
 router.get('/objetivos', IndexController.verObjetivos);
