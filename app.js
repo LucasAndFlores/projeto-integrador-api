db455724d2af6fd123aad635fce4b60da275347a
Caique Bezerra <caique.bezerra@gmail.com>
2021-08-26 20:28:08 -0300

    verificaçao

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3030;
 const models = require('./models');
 const rotasIndex = require('./routes/rotasIndex');
 
