b3e01ac71676379c00c6b1e34d69235f4b71c5e6
Caique Bezerra <caique.bezerra@gmail.com>
2021-08-26 21:21:21 -0300

    corrigidos metodos usuariosController

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -5,40 +5,39 @@ const models = require('../models')
 const usuariosController = {
 
     verConfiguracoes: async (req, res) => {     
-    try{   
-        /* let { id } = req.session.usuario */
-        let usuario = await models.usuarios.findAll({
-        })
-        res.status(200).json(usuario);
-    } catch (error) {
-        res.status(404) 
-        console.log(error)
-    }
+        try{               
+            let usuario = await models.usuarios.findAll({
+            })
+            res.status(200).json(usuario);
+        } catch (error) {
+            res.status(404) 
+            console.log(error)
+        }
     },
 
     editarUsuarios: async (req, res) => {
-    try{
-        const { id } = req.params            
-        const { nome, sobrenome, telefone } = req.body;            
-        let usuario = await models.usuarios.update({ 
-            nome, 
-            sobrenome, 
-            telefone 
-        },
-        {
-            where: {
-                id
-            }
-        });                    
-        /* req.session.save(() => {
-            res.redirect(`/index/configuracoes`) */
-            res.status(200).json(usuario)
-        /* }) */
-    } catch (error) {
-        res.status(404) 
-        console.log(error)
-    }    
-        
+        try{
+            const { id } = req.params            
+            const { nome, sobrenome, telefone } = req.body;            
+            let usuario = await models.usuarios.update({ 
+                nome, 
+                sobrenome, 
+                telefone 
+            },
+            {
+                where: {
+                    id
+                }
+            });                    
+            let mostrandoUsuario = await models.usuarios.findByPk(id)
+
+            res.status(200).json(mostrandoUsuario)
+            
+        } catch (error) {
+            res.status(404) 
+            console.log(error)
+        }    
+            
     },
 
 }
