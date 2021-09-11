81faf5083a3011670b0904defe75588cf76892df
lucasandflores <lcs.oliveira93@gmail.com>
2021-09-10 22:08:04 -0300

    middleware generico com schemascorrigidos

--- routes/rotasCategorias.js
+++ routes/rotasCategorias.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const categoriaController = require('../controllers/CategoriasController');
-const schemas = require('../middlewares/schemasMid')
-const categoriaMiddleware = require('../middlewares/categoriaMiddleware')
+const schemas = require('../middlewares/schemasMiddleware')
+const middlewareValidacao = require ('../middlewares/middlewareValidacao')
 
 router.get('/', categoriaController.todasCategoria);
-router.post('/', categoriaMiddleware(schemas.categoria),categoriaController.cadastrarCategoria);
-router.put('/:id',categoriaMiddleware(schemas.categoria),categoriaController.atualizarCategoria);
+router.post('/', middlewareValidacao(schemas.categoria),categoriaController.cadastrarCategoria);
+router.put('/:id',middlewareValidacao(schemas.categoria),categoriaController.atualizarCategoria);
 router.delete('/:id', categoriaController.destruirCategoria); 
 
 module.exports = router;
