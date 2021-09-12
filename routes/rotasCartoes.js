48c8e361637e473f9e7a46d45ae56de00e3558a7
root <eli melo>
2021-09-11 21:40:37 -0300

    colocando todas as rotas de cartoes

--- routes/rotasCartoes.js
+++ routes/rotasCartoes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const cartoesController = require('../controllers/cartoesController')
+const cartoesController = require('../controllers/CartoesController')
 
 router.get('/',cartoesController.verCartoes);
 router.post('/', cartoesController.criarCartao);
