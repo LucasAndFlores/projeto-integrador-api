8d05102fdb049a78ac6e1790a80229af86e34511
root <eli melo>
2021-09-24 23:44:52 -0300

    modificacoes na migration de cartoes e rotas em jwt

--- migrations/20210812230902-adicionar_colunas_fk.js
+++ migrations/20210812230902-adicionar_colunas_fk.js
@@ -1,18 +1,29 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    queryInterface.addColumn('transacoes', 'fk_categoria_id', {
-      type: Sequelize.INTEGER,
-      references: {
-        model: {
-          tableName: 'categoria'
-        }
-      }
-    })
-  },
+    up: async (queryInterface, Sequelize) => {
+        queryInterface.addColumn('transacoes', 'fk_categoria_id', {
+            type: Sequelize.INTEGER,
+            references: {
+                model: {
+                    tableName: 'categoria'
+                }
+            }
+        })
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    queryInterface.removeColumn('transacoes','fk_categoria_id')
-  }
+    up: async (queryInterface, Sequelize) => {
+        queryInterface.addColumn('cartoes', 'fk_usuario_id', {
+            type: Sequelize.INTEGER,
+            references: {
+                model: {
+                    tableName: 'usuarios'
+                }
+            }
+        })
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        queryInterface.removeColumn('transacoes', 'fk_categoria_id')
+    }
 };
