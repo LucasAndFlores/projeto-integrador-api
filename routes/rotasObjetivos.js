11ebc76f3f747d2fef4f2594b5ab83bfa2c06d21
Eli Melo <eli melo>
2021-10-09 14:22:08 -0300

    colocando leitura/escrita de fk de usuario no objetivos

--- routes/rotasObjetivos.js
+++ routes/rotasObjetivos.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const objetivosController = require('../controllers/ObjetivosController');
 
 router.get('/', objetivosController.verObjetivos);
+<<<<<<< Updated upstream
 router.post('/', objetivosController.criarObjetivo);
 router.put('/:id', objetivosController.editarObjetivo);
+=======
+router.get('/:id', objetivosController.buscarObjetivosUsuario);
+router.post('/', middlewareValidacao(schema.objetivos), objetivosController.criarObjetivo);
+router.put('/:id', middlewareValidacao(schema.objetivos), objetivosController.editarObjetivo);
+>>>>>>> Stashed changes
 router.delete('/:id', objetivosController.deletarObjetivo);
 
 module.exports = router;
